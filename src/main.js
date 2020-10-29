b5f2dc99198976ca7e5d3fa45476eca3b3c6ca9e
Arjun Krishna <arjun12345krishna@gmail.com>
2020-10-30 03:28:36 +0530

    Landing or Home Page Design Completed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from "./App.vue"
+import router from './router'
+
+import 'bootstrap'
+import 'popper.js'
+import './assets/app.scss'
+
+import jQuery from 'jquery' 
+window.$ = window.jQuery = jQuery
+
+Vue.component('NavBar', require('./components/NavBar.vue').default);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/sections/Products.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="products" id="products">
+      <div class="container py-5">
+          <h1 class="text-center p-5">Our Products</h1>
+          <div class="row">
+              
+              <div class="col-md-4">
+                  <div class="card product-item">
+                    <img src="/img/products/product1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Surface Book 2</h5>
+                            <p class="card-text">
+                                Complete your device with Office 365 and get 1TB cloud storage, Excel, Word, PowerPoint and more. Select your suite during checkout.
+                            </p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+              </div>
+
+              <div class="col-md-4">
+                  <div class="card product-item">
+                    <img src="/img/products/product2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Surface Laptop 2</h5>
+                            <p class="card-text">
+                               Style and speed. Go beyond the traditional with new Surface Laptop 2. Featuring improved performance and the full Windows 10 Home experience.
+                            </p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+              </div>
+
+              <div class="col-md-4">
+                  <div class="card product-item">
+                    <img src="/img/products/product3.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Surface Studio 2</h5>
+                            <p class="card-text">
+                                    The ultimate creative studio. Let your ideas flow with brilliant color, blazing graphics, faster processors, intuitive tools, and a stunning, adjustable 28” display.
+
+                            </p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+              </div>
+          </div>
+      </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Products",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .products{
+        background: #f2f2f2;
+        width: 100%;
+        height: 100%;
+        margin: 100px auto;
+    }
+
+    img {
+        height: auto;
+        width: 250px;
+        margin: 25px auto 25px auto;
+    }
+</style>
